2dfe5e76c60118ef59cc38d6b940f9e469cece97
MohdJunaid <2020kuec2038@iiitkota.ac.in>
2023-08-13 18:40:08 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState,useEffect } from 'react';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './Pages/home/Home';
+import MovieList from './components/movieList/MovieList';
+import Movie from './Pages/movieDetail/Movie';
+import Footer from './components/header/Footer';
 function App() {
+  
+//   useEffect(() => {
+//     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=29511babc8ba04b1f13e8ed7d71eeb56&language=en-US`)
+//         .then(res => res.json())
+//         .then(data => {
+//             // Do something with the fetched data, such as updating state
+//             console.log(data.results);
+//         });
+// }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="movie/:id" element={<Movie/>} /> 
+          <Route path="movies/:type" element={<MovieList/>} /> 
+          <Route path="/*" element={<h1> error</h1>} />  
+          
+          
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/header/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='text'>Made with Love by Mohd Junaid</div>
+  )
+}
+
+export default Footer
